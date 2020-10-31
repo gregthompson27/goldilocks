615ccde00ba60b12cd535c5186fbfd0ae8b67979
Greg <gregthompson27@gmail.com>
2020-10-31 14:01:02 -0500

    (add) route for dashboard info
    And import router to index.js to add server filepath

--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
-require("dotenv").config();
-require("./db/index.js");
-const PORT = process.env.PORT || 3000;
-const express = require("express");
-const path = require("path");
-const { listingRouter } = require("./db/routes/listingRoutes");
-const { userRouter } = require("./db/routes/userRoutes");
-const { availabilityRouter } = require("./db/routes/availabilityRoutes");
-const { mapRouter } = require("./api/Map");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+require('./db/index.js');
 
-//////////////////////////// MIDDLEWARE ////////////////////////////
+const PORT = process.env.PORT || 3000;
+const { listingRouter } = require('./db/routes/listingRoutes');
+const { userRouter } = require('./db/routes/userRoutes');
+const { availabilityRouter } = require('./db/routes/availabilityRoutes');
+const { dashboardRouter } = require('./db/routes/dashboardRoutes.js');
+const { mapRouter } = require('./api/Map');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(express.json()); // req.body
+app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-//////////////////////////// ROUTES ////////////////////////////
+app.use('/listing', listingRouter);
+app.use('/user', userRouter);
+app.use('/availability', availabilityRouter);
+app.use('/dashboardInfo', dashboardRouter);
 
-app.use("/auth", require("./routes/jwtAuth"));
-app.use("/dashboard", require("./routes/dashboard"));
-app.use("/listing", listingRouter);
-app.use("/user", userRouter);
-app.use("/availability", availabilityRouter);
-app.use("/map", mapRouter);
-app.use(express.static(path.join(__dirname, "../build")));
+app.use('/auth', require('./routes/jwtAuth'));
+app.use('/dashboard', require('./routes/dashboard'));
 
-//////////////////////////// CONFIRM DATABASE CONNECTION ////////////////////////////
+app.use('/map', mapRouter);
+app.use(express.static(path.join(__dirname, '../build')));
+
 app.listen(PORT, () => {
   console.log(`Listening on port :${PORT}!`);
 });
